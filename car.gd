8ae684602628c6a9399bc8b88eafd5c1223c8eef
BejtullaAjeti <ajetibito@gmail.com>
2026-06-25 20:26:43 +0200

    "added some assets"

--- car.gd
+++ car.gd
@@ -1,17 +1,17 @@
 extends CharacterBody2D
 
 var speed: float = 0.0
-var acceleration: float = 200.0
-var brake_power: float = 150.0
-var max_speed: float = 350.0
-var max_reverse_speed: float = 120.0
+var acceleration: float = 350.0
+var brake_power: float = 225.0
+var max_speed: float = 550.0
+var max_reverse_speed: float = 220.0
 var rotation_speed: float = 2.0
 var friction: float = 40.0
 var grip_traction: float = 2.5
-var drift_traction: float = 0.5
-var grip_angle_threshold: float = 8.0
-var max_slip_angle: float = 60.0
-var min_steer_turn_factor: float = 0.25
+var drift_traction: float = 0.85
+var grip_angle_threshold: float = 6.0
+var max_slip_angle: float = 65.0
+var min_steer_turn_factor: float = 0.33
 func _ready() -> void:
 	pass
 	
@@ -42,7 +42,7 @@ func _move(delta: float) -> void:
 	var slip_angle: float = abs(direction.angle_to(velocity))
 	var slip_deg: float = rad_to_deg(slip_angle)
 	var lateral_t: float = clamp((slip_deg - grip_angle_threshold)/ (max_slip_angle - grip_angle_threshold), 0.0, 1.0)
-	var longitudinal_t: float = 1.0 if (Input.is_action_pressed("accelerate") or Input.is_action_pressed("brake")) else 0.0
+	var longitudinal_t: float = 1.0 if (Input.is_action_pressed("accelerate")) else 0.0
 	var combined_t: float = clamp(sqrt(lateral_t * lateral_t + longitudinal_t * longitudinal_t),0.0,1.0)
 	var effective_traction: float = lerp(grip_traction, drift_traction, combined_t)
 	var target_velocity: Vector2 = direction * speed
